fe7edfc154f462c6d14ebd95731e048f90d2ad3a
Shawn Doyle <shawn@opslock.com>
2021-11-13 12:30:11 -0500

    Initial commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,16 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // We get the contract to deploy
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+  await greeter.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
