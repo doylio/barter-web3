b6f646d19113023c2350a9fbf24165f81fe8deb2
Shawn Doyle <shawn@opslock.com>
2021-11-13 12:52:22 -0500

    Setup basic contracts

---
+++ contracts/BarterWalletFactory.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract BarterWalletFactory {
+  constructor() {}
+
+  // TODO: add a function to create a new wallet
+
+  // TODO: add a function to verify a wallet
+}
