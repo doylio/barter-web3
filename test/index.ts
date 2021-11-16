cd22b72a9047e21f6dca4e6a25f5e222d34bf37c
devlyn37 <dev37-2009@hotmail.com>
2021-11-15 22:03:15 -0800

    test: Added demo test using types and utils

--- test/index.ts
+++ test/index.ts
@@ -1,20 +1,37 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { MockERC20, MockERC721 } from "../typechain";
+import {
+  BundleJSON,
+  CoinBundleJSON,
+  NFTBundleJSON,
+  OfferState,
+  TradeOfferArray,
+  TradeOfferArrayToJSON,
+  TradeOfferJSON,
+} from "./utils";
 
 describe("BarterMarket", function () {
+  let ERC20: MockERC20;
+  let ERC721: MockERC721;
+  let owner: SignerWithAddress;
+  let account1: SignerWithAddress;
+  let account2: SignerWithAddress;
+
   beforeEach(async function () {
-    const [owner, account1, account2] = await ethers.getSigners();
+    [owner, account1, account2] = await ethers.getSigners();
 
     // Deploy Mock ERC20 Contract
     const ERC20Factory = await ethers.getContractFactory("MockERC20");
-    const ERC20 = await ERC20Factory.deploy("Mock ERC20", "MERC20", 50000);
+    ERC20 = await ERC20Factory.deploy("Mock ERC20", "MERC20", 50000);
     await ERC20.deployed();
 
     console.log("ERC20 Deployed", ERC20.address);
 
     // Deploy Mock ERC721 Contract
     const ERC721Factory = await ethers.getContractFactory("MockERC721");
-    const ERC721 = await ERC721Factory.deploy("Mock ERC721", "MERC721");
+    ERC721 = await ERC721Factory.deploy("Mock ERC721", "MERC721");
     await ERC721.deployed();
 
     console.log("ERC721 Deployed", ERC721.address);
@@ -34,27 +51,82 @@ describe("BarterMarket", function () {
     for (var i = 0; i < 10; i++) {
       await ERC721.connect(account2).mint();
     }
-
-    this.owner = owner;
-    this.account1 = account1;
-    this.account2 = account2;
-    this.erc721 = ERC721;
-    this.erc20 = ERC20;
   });
 
   it("Account1 should have a balance of 1000 ERC20", async function () {
-    expect(await this.erc20.balanceOf(this.account1.address)).to.equal(1000);
+    expect(await ERC20.balanceOf(account1.address)).to.equal(1000);
   });
 
   it("Account2 should have a balance of 2000 ERC20", async function () {
-    expect(await this.erc20.balanceOf(this.account2.address)).to.equal(2000);
+    expect(await ERC20.balanceOf(account2.address)).to.equal(2000);
   });
 
-  it("Account1 should have a balance of 1000 ERC20", async function () {
-    expect(await this.erc20.balanceOf(this.account1.address)).to.equal(1000);
-  });
+  it("can send a basic offer", async function () {
+    const barterMarketFactory = await ethers.getContractFactory(
+      "BarterWalletFactory"
+    );
+    const barterMarket = await barterMarketFactory.deploy();
+    await barterMarket.deployed();
 
-  it("Account2 should have a balance of 2000 ERC20", async function () {
-    expect(await this.erc20.balanceOf(this.account2.address)).to.equal(2000);
+    // Define Offer
+    const offerCoins: CoinBundleJSON = {
+      amounts: [100],
+      contractAddresses: [ERC20.address],
+    };
+
+    const offerNfts: NFTBundleJSON = {
+      ids: [1],
+      contractAddresses: [ERC721.address],
+    };
+
+    const offerBundle: BundleJSON = {
+      offeredEther: 0,
+      tokens: offerCoins,
+      nfts: offerNfts,
+    };
+
+    // Account 1 allows contract to trade coins and nfts
+    await ERC20.connect(account1).approve(barterMarket.address, 100);
+    await ERC721.connect(account1).setApprovalForAll(
+      barterMarket.address,
+      true
+    );
+
+    // Define Ask
+    const askCoins: CoinBundleJSON = {
+      amounts: [50],
+      contractAddresses: [ERC20.address],
+    };
+
+    const askNfts: NFTBundleJSON = {
+      ids: [15],
+      contractAddresses: [ERC721.address],
+    };
+
+    const askBundle: BundleJSON = {
+      offeredEther: 0,
+      tokens: askCoins,
+      nfts: askNfts,
+    };
+
+    await barterMarket
+      .connect(account1)
+      .createOffer(account2.address, offerBundle, askBundle);
+
+    const offerCount = await barterMarket.offerCount();
+    expect(offerCount).to.equal(1);
+
+    const offer: TradeOfferArray = await barterMarket.offers(0);
+    const offerJSON = TradeOfferArrayToJSON(offer);
+
+    const expectedOffer: TradeOfferJSON = {
+      offerer: account1.address,
+      target: account2.address,
+      offerBundle: offerBundle,
+      askBundle: askBundle,
+      state: OfferState.SENT,
+    };
+
+    expect(offerJSON).to.eql(expectedOffer);
   });
 });
